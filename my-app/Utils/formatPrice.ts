9f870c76ac038ffec518026900896e877b89df68
Ravijangir536 <jangidravi887@gmail.com>
2024-08-19 21:34:27 +0530

    first commit
    date: 19-08-2024

---
+++ my-app/Utils/formatPrice.ts
@@ -0,0 +1,11 @@
+export const formatPrice = (amount: number) => {
+    return new Intl.NumberFormat
+    ("en-US", {
+        style: "currency",
+        currency: "USD"
+    }).format(amount)
+};
+
+export interface Props {
+    [propName : string]: any;
+}
